885efb726dda94dd1b3b33823a2ca4ce962c3941
Mohit Pala <palav@appstate.edu>
2024-11-17 00:03:51 -0500

    works

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,19 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { firebaseConfig } from './firebaseConfig';
+import { firebaseConfig } from './firebaseConfig'; 
+import { connectDataConnectEmulator, getDataConnect } from 'firebase/data-connect';
+import { connectorConfig } from './car-generated';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
-  provideFirebaseApp(() =>
-    initializeApp({
-      "projectId": firebaseConfig.projectId,
-      "appId": firebaseConfig.appId,
-      "storageBucket": firebaseConfig.storageBucket,
-      "apiKey": firebaseConfig.apiKey,
-      "authDomain": firebaseConfig.authDomain,
-      "messagingSenderId": firebaseConfig.messagingSenderId
-    })),
-  provideAuth(() => getAuth())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideRouter(routes),
+    provideFirebaseApp(() => {
+      const app = initializeApp(firebaseConfig);
+      // Need to initialize Data Connect with connectorConfig
+      const dataConnect = getDataConnect(app, connectorConfig);
+      connectDataConnectEmulator(dataConnect, "localhost", 9399);
+      return app;
+    }),
+    provideAuth(() => getAuth())
+  ]
 };
